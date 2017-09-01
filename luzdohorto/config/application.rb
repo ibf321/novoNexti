79d2c7d46eeb580bd7c44ea6ce510b288f2ef75b
Marcus <marcusviniciusmorais@gmail.com>
2017-09-01 17:23:06 -0300

    colocar imagem no login

--- luzdohorto/config/application.rb
+++ luzdohorto/config/application.rb
@@ -1,13 +1,16 @@
 require_relative 'boot'
 
 require 'rails/all'
-default_locale = :'pt-BR'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module LzdH
   class Application < Rails::Application
+  	config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ["pt-BR"]
+    config.i18n.default_locale = :'pt-BR'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- luzdohorto/config/environments/production.rb
+++ luzdohorto/config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #permitir imagens
+  config.serve_static_assets = true
+  config.assets.digest = true
+
 end
