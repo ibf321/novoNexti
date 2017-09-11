0759736990c328fe92740f21fa0434865fade0a4
Marcus <marcusviniciusmorais@gmail.com>
2017-09-11 14:48:57 -0300

    alteção de alguns layouts

--- luzdohorto/app/models/colab.rb
+++ luzdohorto/app/models/colab.rb
@@ -7,7 +7,7 @@ class Colab < ApplicationRecord
   	if !data_inicio.blank? and !data_final.blank? and verificaData(data_inicio, data_final)
       puts "ESTE TESTE"
       puts "#{Colab.where(created_at: data_inicio.to_date.beginning_of_day..data_final.to_date.end_of_day).as_json}"
-     Colab.find_by_sql('select * from colabs where created_at between data_inicio and data_final, count(colaborador_id), colaboradors.nomecompleto from colabs join colaboradors on colabs.colaborador_id = colaboradors.id group by colaboradors.nomecompleto')
+     Colab.where(created_at: (data_inicio)..data_final).find_by_sql('select count(colabs.id), colaboradors.nomecompleto from colabs join colaboradors on colabs.colaborador_id = colaboradors.id group by colaboradors.nomecompleto')
      
     else
       Colab.find_by_sql('select count(colabs.id), colaboradors.nomecompleto from colabs join colaboradors on colabs.colaborador_id = colaboradors.id group by colaboradors.nomecompleto')
