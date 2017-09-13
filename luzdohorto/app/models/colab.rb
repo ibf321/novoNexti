554869901ef7c3dc0f891dfa9cf9bf76034a7c22
Marcus <marcusviniciusmorais@gmail.com>
2017-09-13 17:21:28 -0300

    alterado layout do botao dos relatorios

--- luzdohorto/app/models/colab.rb
+++ luzdohorto/app/models/colab.rb
@@ -4,9 +4,11 @@ class Colab < ApplicationRecord
 
   def self.buscaColabs (data_inicio, data_final)
 
-  	if !data_inicio.blank? and !data_final.blank? and verificaData(data_inicio, data_final)    
-      Colab.find_by_sql(["SELECT count(colaboradors.id), colaboradors.nomecompleto FROM colabs FULL OUTER JOIN colaboradors  ON colabs.colaborador_id = colaboradors.id where colabs.created_at between '#{data_inicio}' and '#{data_final}' group by colaboradors.nomecompleto"])
-    else
+  	if !data_inicio.blank? and !data_final.blank? and verificaData(data_inicio, data_final) and data_inicio != data_final
+     Colab.find_by_sql(["SELECT count(colaboradors.id), colaboradors.nomecompleto FROM colabs FULL OUTER JOIN colaboradors  ON colabs.colaborador_id = colaboradors.id where colabs.created_at between '#{data_inicio}' and '#{data_final}' group by colaboradors.nomecompleto"])
+    elsif !data_inicio.blank? and !data_final.blank? and data_inicio == data_final
+       Colab.find_by_sql(["SELECT count(colaboradors.id), colaboradors.nomecompleto FROM colabs FULL OUTER JOIN colaboradors  ON colabs.colaborador_id = colaboradors.id where colabs.created_at = '#{data_inicio}'  group by colaboradors.nomecompleto"])    
+    else      
       Colab.find_by_sql('select count(colabs.id), colaboradors.nomecompleto from colabs join colaboradors on colabs.colaborador_id = colaboradors.id group by colaboradors.nomecompleto')
     end
   end
