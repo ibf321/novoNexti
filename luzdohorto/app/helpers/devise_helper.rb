78c5bd25e946e31c49079c7ce7991630c1f58c26
Marcus <marcusviniciusmorais@gmail.com>
2017-09-14 17:08:42 -0300

    implementação das restriçÕes nos models

--- luzdohorto/app/helpers/devise_helper.rb
+++ luzdohorto/app/helpers/devise_helper.rb
@@ -4,15 +4,17 @@ module DeviseHelper
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     html = <<-HTML
-    <div class="alert alert-danger alert-dismissible fade show" role="alert" style="width: 700px;>
-      <button type="button" class="close" data-dismiss="alert">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <strong>
-       #{pluralize(resource.errors.count, "erro(s)")} encontrado(s)
-      </strong>
-      #{messages}
-    </div>
+    <center>
+      <div class="alert alert-danger alert-dismissible fade show" role="alert" style="width: 700px;">
+        <button type="button" class="close" data-dismiss="alert">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <strong>
+         #{pluralize(resource.errors.count, "erro(s)")} encontrado(s)
+        </strong>
+        #{messages}
+      </div>
+    </center
     HTML
 
     html.html_safe
