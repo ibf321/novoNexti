e289818e061c6d87076c4df45eab165f2470efcd
Marcus <marcusviniciusmorais@gmail.com>
2017-09-20 16:15:23 -0300

    mudanca na filtragem dos colaboradores

--- luzdohorto/app/controllers/colabs_controller.rb
+++ luzdohorto/app/controllers/colabs_controller.rb
@@ -1,4 +1,5 @@
 class ColabsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_colab, only: [:show, :edit, :update, :destroy]
 
   # GET /colabs
--- luzdohorto/app/models/colab.rb
+++ luzdohorto/app/models/colab.rb
@@ -5,11 +5,20 @@ class Colab < ApplicationRecord
   def self.buscaColabs (data_inicio, data_final)
 
   	if !data_inicio.blank? and !data_final.blank? and verificaData(data_inicio, data_final) and data_inicio != data_final
-     Colab.find_by_sql(["SELECT count(colaboradors.id), colaboradors.nomecompleto FROM colabs FULL OUTER JOIN colaboradors  ON colabs.colaborador_id = colaboradors.id where colabs.created_at between '#{data_inicio}' and '#{data_final}' group by colaboradors.nomecompleto"])
+     Colab.find_by_sql(["select count(colabs.id), cadastros.valor, colaboradors.nomecompleto from colabs 
+                        join colaboradors on colabs.colaborador_id = colaboradors.id 
+                        join cadastros on colabs.cadastro_id = cadastros.id where colabs.created_at between '#{data_inicio}' and '#{data_final}' group by cadastros.valor, colaboradors.nomecompleto order by nomecompleto asc"])
+    
     elsif !data_inicio.blank? and !data_final.blank? and data_inicio == data_final
-       Colab.find_by_sql(["SELECT count(colaboradors.id), colaboradors.nomecompleto FROM colabs FULL OUTER JOIN colaboradors  ON colabs.colaborador_id = colaboradors.id where colabs.created_at = '#{data_inicio}'  group by colaboradors.nomecompleto"])    
+       Colab.find_by_sql(["select count(colabs.id), cadastros.valor, colaboradors.nomecompleto from colabs 
+                        join colaboradors on colabs.colaborador_id = colaboradors.id 
+                        join cadastros on colabs.cadastro_id = cadastros.id where colabs.created_at = '#{data_inicio}'  group by cadastros.valor, colaboradors.nomecompleto order by nomecompleto asc"])    
+    
     else      
-      Colab.find_by_sql('select count(colabs.id), colaboradors.nomecompleto from colabs join colaboradors on colabs.colaborador_id = colaboradors.id group by colaboradors.nomecompleto')
+      Colab.find_by_sql('select count(colabs.id), cadastros.valor, colaboradors.nomecompleto from colabs 
+                        join colaboradors on colabs.colaborador_id = colaboradors.id 
+                        join cadastros on colabs.cadastro_id = cadastros.id
+                        group by cadastros.valor, colaboradors.nomecompleto order by nomecompleto asc')
     end
   end
 
